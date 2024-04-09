f4c915f017a07113a779bf0b92f3def4ba62c727
Julian Benegas <julianbenegas99@gmail.com>
2024-04-09 14:29:57 -0300

    fix build

--- basehub-helpers/fragments.ts
+++ basehub-helpers/fragments.ts
@@ -1,31 +1,21 @@
-import { fragmentOn } from "@/.basehub";
-import { CalloutFragment } from "@/app/_components/article/callout";
-import { HeadingWithIconFragment } from "@/app/_components/article/heading-with-icon";
-
-/* -------------------------------------------------------------------------------------------------
- * Helpers
- * -----------------------------------------------------------------------------------------------*/
-
-type RecursiveCollection<T, Key extends string> = T & {
-  [key in Key]?: { items?: RecursiveCollection<T, Key> };
-};
-
-import { fragmentOn, fragmentOnRecursiveCollection } from "@/.basehub";
+import { fragmentOn, fragmentOnRecursiveCollection } from '@/.basehub'
+import { CalloutFragment } from '@/app/_components/article/callout'
+import { HeadingWithIconFragment } from '@/app/_components/article/heading-with-icon'
 
 /* -------------------------------------------------------------------------------------------------
  * Article
  * -----------------------------------------------------------------------------------------------*/
 
-export const ArticleBodyFragment = fragmentOn("BodyRichText", {
+export const ArticleBodyFragment = fragmentOn('BodyRichText', {
   content: true,
   blocks: {
     __typename: true,
     on_CalloutComponent: CalloutFragment,
     on_HeadingWithIconComponent: HeadingWithIconFragment,
   },
-});
+})
 
-export const ArticleFragment = fragmentOn("ArticleComponent", {
+export const ArticleFragment = fragmentOn('ArticleComponent', {
   _id: true,
   _title: true,
   _slug: true,
@@ -38,36 +28,36 @@ export const ArticleFragment = fragmentOn("ArticleComponent", {
     readingTime: true,
     json: ArticleBodyFragment,
   },
-});
+})
 
-export type ArticleFragment = fragmentOn.infer<typeof ArticleFragment>;
+export type ArticleFragment = fragmentOn.infer<typeof ArticleFragment>
 
 export const ArticleFragmentRecursive = fragmentOnRecursiveCollection(
-  "ArticleComponent",
+  'ArticleComponent',
   ArticleFragment,
   {
     levels: 5,
-    recursiveKey: "children",
+    recursiveKey: 'children',
   }
-);
+)
 
 export type ArticleFragmentRecursive = fragmentOn.infer<
   typeof ArticleFragmentRecursive
->;
+>
 
 /* -------------------------------------------------------------------------------------------------
  * Page
  * -----------------------------------------------------------------------------------------------*/
 
-export const PageFragment = fragmentOn("PagesItem", {
+export const PageFragment = fragmentOn('PagesItem', {
   _slug: true,
   articles: { items: ArticleFragmentRecursive },
-});
+})
 
-export type PageFragment = fragmentOn.infer<typeof PageFragment>;
+export type PageFragment = fragmentOn.infer<typeof PageFragment>
 
 export const pageBySlug = (slug: string | undefined) => {
-  return fragmentOn("Pages", {
+  return fragmentOn('Pages', {
     __args: {
       first: 1,
       ...(slug && {
@@ -75,5 +65,5 @@ export const pageBySlug = (slug: string | undefined) => {
       }),
     },
     items: PageFragment,
-  });
-};
+  })
+}
