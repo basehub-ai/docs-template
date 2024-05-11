7f6aae445927731c7fb597f3ddfdf830ed9ad572
Julian Benegas <julianbenegas99@gmail.com>
2024-05-10 21:18:34 -0400

    it works

--- app/_components/header/index.tsx
+++ app/_components/header/index.tsx
@@ -2,10 +2,23 @@ import { Pump } from '@/.basehub/react-pump'
 import { HeaderFragment, PagesNav } from './pages-nav'
 import NextLink from 'next/link'
 import { draftMode } from 'next/headers'
-import { Box, Button, Container, Flex, Grid, Link, Text } from '@radix-ui/themes'
+import {
+  Box,
+  Button,
+  Container,
+  Dialog,
+  Flex,
+  Grid,
+  IconButton,
+  Kbd,
+  Link,
+  Text,
+  TextField,
+} from '@radix-ui/themes'
 import { ThemeSwitcher } from '../theme-switcher'
-import { Search } from './search'
+import { SearchProvider } from './search'
 import { Logo } from '../logo'
+import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
 
 import s from './header.module.scss'
 
@@ -15,91 +28,132 @@ export const Header = () => {
       queries={[
         { settings: { logo: { url: true }, logoDark: { url: true } } },
         { header: HeaderFragment },
+        {
+          _componentInstances: {
+            article: { _searchKey: true },
+          },
+        },
       ]}
       next={{ revalidate: 30 }}
       draft={draftMode().isEnabled}
     >
-      {async ([{ settings }, { header }]) => {
+      {async ([
+        { settings },
+        { header },
+        {
+          _componentInstances: {
+            article: { _searchKey },
+          },
+        },
+      ]) => {
         'use server'
+
         const logoLight = settings.logo.url
         const logoDark = settings.logoDark.url
 
         return (
-          <header className={s.header}>
-            <Container
-              size="4"
-              px={{ initial: '5', md: '8' }}
-              height="100%"
-              className={s['header__head']}
-              position="relative"
-            >
-              {/* mobile */}
-              <Grid
-                columns="3"
-                align="center"
+          <SearchProvider
+            _searchKey={_searchKey}
+            searchCategories={header.navLinks.items}
+          >
+            <header className={s.header}>
+              <Container
+                size="4"
+                px={{ initial: '5', md: '8' }}
                 height="100%"
-                justify="between"
-                display={{ initial: 'none', md: 'grid' }}
+                className={s['header__head']}
+                position="relative"
               >
-                <Logo logoLight={logoLight} logoDark={logoDark} />
-                <Flex maxWidth="300px" mx="auto">
-                  <Search searchCategories={header.navLinks.items} />
+                {/* mobile */}
+                <Flex
+                  align="center"
+                  height="100%"
+                  justify="between"
+                  display={{ initial: 'flex', md: 'none' }}
+                >
+                  <Flex align="center" justify="end">
+                    <Logo logoLight={logoLight} logoDark={logoDark} />
+                    <Box ml="3">
+                      <Dialog.Trigger>
+                        <IconButton variant="soft">
+                          <MagnifyingGlassIcon />
+                        </IconButton>
+                      </Dialog.Trigger>
+                    </Box>
+                  </Flex>
+                  <Flex align="center" justify="end">
+                    <ThemeSwitcher />
+                    <Button
+                      asChild
+                      ml="5"
+                      radius="large"
+                      className={s['header__main-cta']}
+                      size="2"
+                    >
+                      <Link asChild>
+                        <NextLink href="#">
+                          <Text as="span" weight="medium">
+                            Main CTA
+                          </Text>
+                        </NextLink>
+                      </Link>
+                    </Button>
+                  </Flex>
                 </Flex>
-                <Flex align="center" justify="end">
-                  <ThemeSwitcher />
-                  <Button
-                    asChild
-                    ml="5"
-                    radius="large"
-                    className={s['header__main-cta']}
-                    size="2"
-                  >
-                    <Link asChild>
-                      <NextLink href="#">
-                        <Text as="span" weight="medium">
-                          Main CTA
-                        </Text>
-                      </NextLink>
-                    </Link>
-                  </Button>
-                </Flex>
-              </Grid>
 
-              {/* >= md */}
-              <Flex
-                align="center"
-                height="100%"
-                justify="between"
-                display={{ initial: 'flex', md: 'none' }}
-              >
-                <Flex align="center" justify="end">
+                {/* desktop */}
+                <Grid
+                  columns="3"
+                  align="center"
+                  height="100%"
+                  justify="between"
+                  display={{ initial: 'none', md: 'grid' }}
+                >
                   <Logo logoLight={logoLight} logoDark={logoDark} />
-                  <Box ml="3">
-                    <Search searchCategories={header.navLinks.items} />
-                  </Box>
-                </Flex>
-                <Flex align="center" justify="end">
-                  <ThemeSwitcher />
-                  <Button
-                    asChild
-                    ml="5"
-                    radius="large"
-                    className={s['header__main-cta']}
-                    size="2"
-                  >
-                    <Link asChild>
-                      <NextLink href="#">
-                        <Text as="span" weight="medium">
-                          Main CTA
-                        </Text>
-                      </NextLink>
-                    </Link>
-                  </Button>
-                </Flex>
-              </Flex>
-            </Container>
-            <PagesNav />
-          </header>
+                  <Flex maxWidth="300px" mx="auto" width="100%">
+                    <Dialog.Trigger style={{ width: '100%' }}>
+                      <TextField.Root
+                        readOnly
+                        placeholder="Search"
+                        size="2"
+                        radius="large"
+                        className={s['search-trigger']}
+                      >
+                        <TextField.Slot>
+                          <MagnifyingGlassIcon color="currentColor" />
+                        </TextField.Slot>
+                        <TextField.Slot>
+                          <Flex align="center" justify="between" gap="1">
+                            <Kbd>⌘</Kbd>
+                            <Kbd>k</Kbd>
+                          </Flex>
+                        </TextField.Slot>
+                      </TextField.Root>
+                    </Dialog.Trigger>
+                  </Flex>
+                  <Flex align="center" justify="end">
+                    <ThemeSwitcher />
+                    <Button
+                      asChild
+                      ml="5"
+                      radius="large"
+                      className={s['header__main-cta']}
+                      size="2"
+                    >
+                      <Link asChild>
+                        <NextLink href="#">
+                          <Text as="span" weight="medium">
+                            Main CTA
+                          </Text>
+                        </NextLink>
+                      </Link>
+                    </Button>
+                  </Flex>
+                </Grid>
+              </Container>
+              <PagesNav />
+            </header>
+          </SearchProvider>
         )
       }}
     </Pump>
--- app/_components/header/search/index.tsx
+++ app/_components/header/search/index.tsx
@@ -1,13 +1,13 @@
 'use client'
 
 import * as React from 'react'
+import { useSearch, SearchBox } from 'basehub/react-search'
 import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
 import {
   Box,
   Card,
   Dialog,
   Flex,
-  IconButton,
   Kbd,
   Link,
   Select,
@@ -16,60 +16,35 @@ import {
   TextField,
 } from '@radix-ui/themes'
 import NextLink from 'next/link'
-import { usePointerIdle } from '@/hooks/use-pointer-idle'
 import { HeaderFragment } from '../pages-nav'
 
 import s from './search.module.scss'
 
-const SEARCH_RESULTS = [
-  'Getting started',
-  'Guides',
-  'Components',
-  'API',
-  'Examples',
-  'More info',
-  'Setup',
-  'API Reference',
-  'Tests',
-  'Integrations',
-  'Routing',
-]
-
-export const Search = ({
+export const SearchProvider = ({
+  _searchKey,
   searchCategories,
+  children,
 }: {
+  _searchKey: string | null
   searchCategories: HeaderFragment['navLinks']['items']
+  children: React.ReactNode
 }) => {
-  const inputRef = React.useRef<HTMLInputElement>(null)
   const [open, setOpen] = React.useState(false)
+
+  const search = useSearch({
+    _searchKey,
+    queryBy: ['_title', 'body'],
+  })
 
   React.useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === 'k' && event.metaKey) {
-        event.preventDefault()
-        setOpen((o) => !o)
-      }
-    }
-
-    document.addEventListener('keydown', handleKeyDown)
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown)
-    }
-  }, [])
-
-  React.useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (
-        event.key === 'Enter' &&
-        document.activeElement === inputRef.current
-      ) {
         event.preventDefault()
         setOpen(true)
       }
     }
 
     document.addEventListener('keydown', handleKeyDown)
-
     return () => {
       document.removeEventListener('keydown', handleKeyDown)
     }
@@ -77,137 +52,60 @@ export const Search = ({
 
   return (
     <Dialog.Root open={open} onOpenChange={setOpen}>
-      {/* mobile */}
-      <Dialog.Trigger tabIndex={-1}>
-        <Box display={{ sm: 'none' }}>
-          <IconButton variant="soft">
-            <MagnifyingGlassIcon />
-          </IconButton>
-        </Box>
-      </Dialog.Trigger>
-
-      {/* >= md */}
-      <Dialog.Trigger>
-        <Box display={{ initial: 'none', sm: 'block' }}>
-          <TextField.Root
-            ref={inputRef}
-            readOnly
-            placeholder="Search"
-            size="2"
-            radius="large"
-            onClick={() => setOpen(true)}
-            className={s['search-trigger']}
-          >
-            <TextField.Slot>
-              <MagnifyingGlassIcon color="currentColor" />
-            </TextField.Slot>
-            <TextField.Slot>
-              <Flex align="center" justify="between" gap="1">
-                <Kbd>⌘</Kbd>
-                <Kbd>k</Kbd>
-              </Flex>
-            </TextField.Slot>
-          </TextField.Root>
-        </Box>
-      </Dialog.Trigger>
-
-      <DialogContent searchCategories={searchCategories} />
+      {children}
+      <SearchBox.Root search={search}>
+        <DialogContent
+          searchCategories={searchCategories}
+          onClose={() => {
+            setOpen(false)
+          }}
+        />
+      </SearchBox.Root>
     </Dialog.Root>
   )
 }
 
 const DialogContent = ({
   searchCategories,
+  onClose,
 }: {
   searchCategories: HeaderFragment['navLinks']['items']
+  onClose: () => void
 }) => {
-  const searchResultsRef = React.useRef<HTMLDivElement>(null)
-  const pointerIdle = usePointerIdle(1000)
-  const [selectedResultIndex, setSelectedResultIndex] = React.useState(0)
-  const [search, setSearch] = React.useState('')
+  const search = SearchBox.useContext()
+
   const [selectedSearchCategoryId, setSelectedSearchCategoryId] =
     React.useState(searchCategories?.[0]?._id ?? '')
 
-  React.useEffect(() => {
-    if (!searchResultsRef.current) return
-
-    const selectedResultAnchor = searchResultsRef.current.querySelector(
-      `a[data-index="${selectedResultIndex}"]`
-    ) as HTMLElement
-
-    const selectedResultElement = selectedResultAnchor.parentElement
-
-    if (!selectedResultElement) return
-    const containerHeight = searchResultsRef.current.offsetHeight
-    const containerScrollTop = searchResultsRef.current.scrollTop
-    const elementOffsetTop = selectedResultElement.offsetTop
-    const elementOffsetHeight = selectedResultElement.offsetHeight
-    const containerPaddingTop = parseInt(
-      window.getComputedStyle(searchResultsRef.current).paddingTop
-    )
-    const containerPaddingBottom = parseInt(
-      window.getComputedStyle(searchResultsRef.current).paddingBottom
-    )
-
-    if (
-      elementOffsetTop < containerScrollTop + containerPaddingTop ||
-      elementOffsetTop + elementOffsetHeight >
-        containerScrollTop + containerHeight - containerPaddingBottom
-    ) {
-      selectedResultElement.scrollIntoView({ block: 'nearest' })
-    }
-  }, [selectedResultIndex])
-
-  const results = SEARCH_RESULTS.filter((item) => item.includes(search))
   const selectedCat = searchCategories.find(
     (category) => category._id === selectedSearchCategoryId
   )
+
   const selectedCategoryLabel =
     selectedCat?.label ?? selectedCat?.page?._title ?? 'Untitled Category'
 
   return (
     <Dialog.Content maxWidth="550px" className={s['search-dialog__content']}>
       <Flex direction="column" height="100%">
-        <TextField.Root
-          placeholder="Search"
-          mx="2"
-          mt="2"
-          size="3"
-          onChange={(e) => setSearch(e.target.value)}
-          onKeyDown={(e) => {
-            if (!['ArrowDown', 'ArrowUp'].includes(e.key)) return
-
-            setSelectedResultIndex((_selectedResultIndex) => {
-              if (
-                e.key === 'ArrowDown' &&
-                _selectedResultIndex < SEARCH_RESULTS.length - 1
-              ) {
-                return _selectedResultIndex + 1
-              } else if (e.key === 'ArrowUp' && _selectedResultIndex > 0) {
-                return _selectedResultIndex - 1
-              }
-
-              return _selectedResultIndex
-            })
-          }}
-        >
-          <TextField.Slot>
-            <MagnifyingGlassIcon color="currentColor" />
-          </TextField.Slot>
-          <TextField.Slot>
-            <Kbd style={{ marginBlock: 'auto' }}>Esc</Kbd>
-          </TextField.Slot>
-        </TextField.Root>
+        <SearchBox.Input asChild>
+          <TextField.Root placeholder="Search" mx="2" mt="2" size="3">
+            <TextField.Slot>
+              <MagnifyingGlassIcon color="currentColor" />
+            </TextField.Slot>
+            <TextField.Slot>
+              <Kbd style={{ marginBlock: 'auto' }}>Esc</Kbd>
+            </TextField.Slot>
+          </TextField.Root>
+        </SearchBox.Input>
         <Separator size="4" mt="2" />
         <Box
           className={s['search-dialog__results']}
-          ref={searchResultsRef}
           flexGrow="1"
           flexShrink="1"
           flexBasis="0%"
           p="2"
         >
-          {!results.length ? (
+          <SearchBox.Empty asChild>
             <Flex align="center" px="2" py="1">
               <Text
                 size="2"
@@ -216,41 +114,95 @@ const DialogContent = ({
               >
                 No results for{' '}
                 <Text as="span" weight="bold">
-                  &ldquo;{search}&rdquo;
+                  &ldquo;{search.query}&rdquo;
                 </Text>
               </Text>
             </Flex>
-          ) : (
-            results.map((item, index) => {
+          </SearchBox.Empty>
+          <SearchBox.Placeholder asChild>
+            <Flex align="center" px="2" py="1">
+              <Text
+                size="2"
+                color="gray"
+                className={s['search-dialog__empty-state']}
+              >
+                Start typing to search...
+              </Text>
+            </Flex>
+          </SearchBox.Placeholder>
+          <SearchBox.HitsList>
+            {search.result?.hits.map((hit, index) => {
+              let pathname =
+                // TODO add this function to utils or something as it's being used in the draft enable route.
+
+                // _slugPath will have something like root/pages/<category>/articles/<page>/children/<page>/children/<page>...
+                // remove root/pages and then filter out every other part
+                '/' +
+                hit.document._slugPath
+                  ?.replace('root/pages/', '')
+                  .split('/')
+                  .filter((_part, index) => {
+                    return index % 2 === 0
+                  })
+                  .join('/')
+
+              // TODO is there an opportunity to build a helper function in our SDK here? looks like a common usecase
+              const bodyHighlight = hit.highlights
+                .map((h) => {
+                  if (h.fieldPath.startsWith('body') === false) return
+                  const splitted = h.fieldPath.split('.').slice(0, 2)
+                  const fullField = hit._getField(splitted.join('.'))
+                  return fullField
+                })
+                .filter(Boolean)[0] as { _id: string | undefined } | undefined
+
+              if (bodyHighlight?._id) {
+                pathname += `#${bodyHighlight._id}`
+              }
+
               return (
-                <Box key={index} className={s['search-dialog-content__result']}>
+                <Box
+                  key={hit._key}
+                  className={s['search-dialog-content__result']}
+                >
                   <Card variant="ghost" size="2" asChild>
                     <Link asChild>
-                      <NextLink
-                        data-index={index}
-                        data-selected={selectedResultIndex === index}
-                        onMouseOver={() => {
-                          if (pointerIdle) return
-                          setSelectedResultIndex(index)
-                        }}
-                        tabIndex={-1}
-                        className={s['search-dialog-content__result-link']}
-                        href="#"
-                      >
-                        <Text size="2" weight="medium">
-                          {item}
-                        </Text>
-                        <Text size="1" as="p" color="gray" mt="1">
-                          Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-                          amet.
-                        </Text>
-                      </NextLink>
+                      <SearchBox.HitItem hit={hit} href={pathname} asChild>
+                        <NextLink
+                          data-index={index}
+                          tabIndex={-1}
+                          className={s['search-dialog-content__result-link']}
+                          href={pathname}
+                          onClick={onClose}
+                        >
+                          <SearchBox.HitSnippet
+                            fieldPath="_title"
+                            components={{
+                              container: ({ children }) => (
+                                <Text size="2" weight="medium">
+                                  {children}
+                                </Text>
+                              ),
+                            }}
+                          />
+                          <SearchBox.HitSnippet
+                            fieldPath="body"
+                            components={{
+                              container: ({ children }) => (
+                                <Text size="1" as="p" color="gray" mt="1">
+                                  {children}
+                                </Text>
+                              ),
+                            }}
+                          />
+                        </NextLink>
+                      </SearchBox.HitItem>
                     </Link>
                   </Card>
                 </Box>
               )
-            })
-          )}
+            })}
+          </SearchBox.HitsList>
         </Box>
         {searchCategories.length && (
           <>
