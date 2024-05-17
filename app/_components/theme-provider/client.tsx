70a1ee37387676c741b20fb050e2ba132304638e
Julian Benegas <julianbenegas99@gmail.com>
2024-05-17 14:56:50 -0400

    fix theme syncing

--- app/_components/theme-provider/client.tsx
+++ app/_components/theme-provider/client.tsx
@@ -4,6 +4,7 @@ import { useThemeContext } from '@radix-ui/themes'
 import { ThemeSettingsFragment } from './server'
 import * as React from 'react'
 import { useTheme } from 'next-themes'
+import { useHasRendered } from '@/hooks/use-has-rendered'
 
 export const LiveThemeSwitcher = ({
   theme,
@@ -18,15 +19,21 @@ export const LiveThemeSwitcher = ({
     onAppearanceChange,
   } = useThemeContext()
   const { setTheme } = useTheme()
+  const hasRendered = useHasRendered()
 
   React.useEffect(() => {
+    // wait for first render to happen before re-syncing theme
+    if (!hasRendered) return
     onAccentColorChange(theme.accentColor as any)
     onRadiusChange(theme.radius as any)
     onGrayColorChange(theme.grayScale as any)
     onAppearanceChange(theme.appearance as any)
     onScalingChange(theme.scaling as any)
     setTheme((p) => {
-      const changed = p !== theme.appearance
+      const changed =
+        p !== theme.appearance &&
+        theme.appearance !== 'inherit' &&
+        theme.appearance !== 'system'
       if (!changed) return p
       if (!theme.appearance || theme.appearance === 'inherit') return 'system'
       return theme.appearance
@@ -43,6 +50,7 @@ export const LiveThemeSwitcher = ({
     onRadiusChange,
     onScalingChange,
     setTheme,
+    hasRendered,
   ])
 
   return null
