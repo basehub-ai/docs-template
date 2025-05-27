3694ffee38aa436e0ef38f1ea9822f4da4925430
Fabroos <signorettafabrizio@gmail.com>
2025-05-27 18:08:11 -0300

    Fix revalidate xml

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const siteHost =
+  process.env.VERCEL_PROJECT_PRODUCTION_URL || 'docs.basehub.com'
+export const siteUrl = `https://${siteHost}`
--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-let siteHost = process.env.VERCEL_PROJECT_PRODUCTION_URL || 'docs.basehub.com'
-let siteUrl = `https://${siteHost}`
-
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl,
-  generateRobotsTxt: true,
-}
