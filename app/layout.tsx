f0d1d3aa755c1670f9a5bb43e88e1920bdbac50d
Julian Benegas <julianbenegas99@gmail.com>
2025-06-29 00:34:33 -0300

    agent embed

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { GeistMono } from 'geist/font/mono'
 import { ThemeProvider } from './_components/theme-provider/server'
 import { Toolbar } from 'basehub/next-toolbar'
 import '../basehub.config'
+import Script from 'next/script'
 
 import './globals.css'
 
@@ -20,6 +21,8 @@ export default function RootLayout({
           <Footer />
         </ThemeProvider>
         <Toolbar />
+
+        <Script src="https://agent.basehub.com/main/HfmlGB2hmtEx4KB1hnewD/embed.js?session=eyJhbGciOiJIUzI1NiJ9.eyJ0b2tlbiI6ImJzaGJfcGtfeGtiNWhyN3p4Nm0wdXU4ZDRmYXJrYXJkZnllbmdyanYwdzExYTFtYTJzeDk3ZjFpejR1Njd0eTN3eTY2azRzdyIsInJlcG9JZCI6IlJ6d2N3R0tTaGNCMHBCbU9pVXNlWSIsImFnZW50SWQiOiJIZm1sR0IyaG10RXg0S0IxaG5ld0QifQ.e5blrlXzj-HtaQ5j--TIjZadKqaVsw20xiPCOeA2h8k" />
       </body>
     </html>
   )
