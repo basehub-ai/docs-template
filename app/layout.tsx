ad15c0b3eaa1788ab60899729276ee4448b86e81
Julian Benegas <julianbenegas99@gmail.com>
2025-06-29 13:04:55 -0300

    upgra

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,23 @@ import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { ThemeProvider } from './_components/theme-provider/server'
 import { Toolbar } from 'basehub/next-toolbar'
-import '../basehub.config'
 import Script from 'next/script'
+import { basehub } from 'basehub'
 
+import '../basehub.config'
 import './globals.css'
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const data = await basehub().query({
+    _agents: {
+      docsAi: {
+        embedUrl: true,
+      },
+    },
+  })
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${GeistSans.variable} ${GeistMono.variable} font-sans`}>
@@ -22,7 +31,7 @@ export default function RootLayout({
         </ThemeProvider>
         <Toolbar />
 
-        <Script src="https://agent.basehub.com/main/HfmlGB2hmtEx4KB1hnewD/embed.js?session=eyJhbGciOiJIUzI1NiJ9.eyJ0b2tlbiI6ImJzaGJfcGtfeGtiNWhyN3p4Nm0wdXU4ZDRmYXJrYXJkZnllbmdyanYwdzExYTFtYTJzeDk3ZjFpejR1Njd0eTN3eTY2azRzdyIsInJlcG9JZCI6IlJ6d2N3R0tTaGNCMHBCbU9pVXNlWSIsImFnZW50SWQiOiJIZm1sR0IyaG10RXg0S0IxaG5ld0QifQ.e5blrlXzj-HtaQ5j--TIjZadKqaVsw20xiPCOeA2h8k" />
+        <Script src={data._agents.docsAi.embedUrl} />
       </body>
     </html>
   )
